59700fdd8cd6ed726ba2200fcb0a8240cf884643
rajsekharde <rajsekharde612@gmail.com>
2026-01-09 14:46:06 +0530

    Add showDownloads() in js

--- frontend/app.js
+++ frontend/app.js
@@ -28,3 +28,28 @@ async function fetchFileStatus(jobId) {
         }
     }
 }
+
+async function showDownloads(jobId, files) {
+    const container = document.createElement("div");
+    container.className = "testDiv"
+    container.innerHTML = "<h3>Results</h3>";
+
+    for (const file of files) {
+        const button = document.createElement("button");
+        button.textContent = "Download";
+
+        button.addEventListener("click", () => {
+            const link = document.createElement("a");
+            link.href = `http://api-f:8000/download/${jobId}/${encodeURIComponent(file)}`;
+            link.download = file;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
+        button.style.display = "block";
+
+        container.appendChild(button);
+    }
+
+    document.body.appendChild(container);
+}
