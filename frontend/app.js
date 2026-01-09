a7d529dfce2d1e44b10d90b7f35868c60747f7ef
rajsekharde <rajsekharde612@gmail.com>
2026-01-09 14:26:39 +0530

    Add uploadFile() in js

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,17 @@
-function uploadFile() {
-    
+async function uploadFile() {
+    const file = document.getElementById("fileInput").files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch("http://lapi-f:8000/upload", {
+        method: "POST",
+        body: formData
+    });
+
+    const data = await res.json();
+    console.log("UPLOAD:", data);
+
+    watchJob(data.job_id);
 }
