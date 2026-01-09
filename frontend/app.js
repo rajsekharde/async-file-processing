db470faedcffd116795d43d313366c8e15f7df3a
rajsekharde <rajsekharde612@gmail.com>
2026-01-09 14:34:54 +0530

    Add fetchFileStatus() in js

--- frontend/app.js
+++ frontend/app.js
@@ -5,7 +5,7 @@ async function uploadFile() {
     const formData = new FormData();
     formData.append("file", file);
 
-    const res = await fetch("http://lapi-f:8000/upload", {
+    const res = await fetch("http://api-f:8000/upload", {
         method: "POST",
         body: formData
     });
@@ -14,4 +14,17 @@ async function uploadFile() {
     console.log("UPLOAD:", data);
 
     watchJob(data.job_id);
+}
+
+async function fetchFileStatus(jobId) {
+    while (true) {
+        await new Promise(r => setTimeout(r, 2000));
+
+        const res = await fetch(`http://api-f:8000/status/${jobId}`);
+        const data = await res.json();
+
+        if (data.status === "COMPLETED") {
+            showDownloads(jobId, data.files);
+        }
+    }
 }
