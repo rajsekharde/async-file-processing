00e621d4c4d8eed88e609e91ef11abb5f6993a45
rajsekharde <rajsekharde612@gmail.com>
2026-01-09 14:20:24 +0530

    Add test file input field in index

--- frontend/app.js
+++ frontend/app.js
@@ -1,18 +1,3 @@
-function sendText() {
-    const userText = document.getElementById("inputTest").value;
-
-    fetch('http://localhost:8000/test', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text: userText }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+function uploadFile() {
+    
 }
